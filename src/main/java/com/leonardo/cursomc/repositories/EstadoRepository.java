4761150fe1703fc1ec4b874af452a7b04de7b3a5
Leonardo Mello <leonardomello22@gmail.com>
2018-08-17 13:28:53 -0300

    Estados e Cidades

---
+++ src/main/java/com/leonardo/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,9 @@
+package com.leonardo.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.leonardo.cursomc.domain.Estado;
+
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+}
